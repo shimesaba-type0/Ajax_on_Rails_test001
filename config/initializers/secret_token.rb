29692e3c8e6af5f1b09c8bb61967d7de69b443b4
shimesaba-type0 <shimesaba@shimesaba-top.(none)>
2013-08-06 22:34:58 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AjaxOnRailsTest001::Application.config.secret_token = '4b7094b5bcfab47cc74c7c46933cb162d5d13a3e6a1c461dc9283a0945484d660760cd0f4960f064995c7cd195e72a23503104e578d1d46d4c5aa51220a5c75a'
