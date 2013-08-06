ce84b295f80be424c578738190bcb0b7c22a5148
shimesaba-type0 <shimesaba@shimesaba-top.(none)>
2013-08-06 23:08:36 +0900

    validationを追加。user_nameとmail_addressに。

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
   attr_accessible :id, :mail_address, :password, :user_id, :user_name
+
+  validates :user_name, :presence => true, 
+    :length => {:minimum => 2, :maximum => 10}
+
+  validates :mail_address, :format => {:with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i}
 end
